0eca8557884cad8f654c51505c38c10ade2cc1f9
Adam <adamb56789@gmail.com>
2026-02-15 09:58:41 +0000

    refactor again and add tests for rearrangement

--- rummi.py
+++ rummi.py
@@ -1,6 +1,7 @@
 import itertools
 from dataclasses import dataclass
 from enum import Enum
+from typing import Optional
 
 import numpy as np
 import pandas as pd
@@ -37,201 +38,225 @@ class Tile:
 
 
 class MaximizeMode(Enum):
-    TILES_PLACED = 1
-    VALUE_PLACED = 2
+    TILES_PLACED = "tiles_placed"
+    VALUE_PLACED = "value_placed"
 
 
 class JokerMode(Enum):
-    LOCKING = 1
-    FREE = 2
+    LOCKING = "locking"
+    FREE = "free"
+
+
+@dataclass
+class Config:
+    joker_mode: JokerMode
+    maximize_mode: MaximizeMode
+    joker_value: Optional[int] = None
+    rearrange_value: float = 1 / 40
+
+    def __post_init__(self):
+        if self.maximize_mode == MaximizeMode.VALUE_PLACED and self.joker_value is None:
+            raise Exception("Joker value must be set")
+
+
+@dataclass
+class RummiResult:
+    table: list[tuple[Tile, ...]]
+    placed: list[Tile]
+    remaining: list[Tile]
+
+
+def create_runs():
+    runs: list[tuple[Tile, ...]] = []
+    for c in COLOURS:
+        for run_length in [3, 4, 5]:
+
+            # No Jokers
+
+            for i in range(1, 15 - run_length):
+                runs.append(tuple(Tile(c, j) for j in range(i, i + run_length)))
+
+            # One joker
+
+            # Place jokers starting from the 2nd position, since using the first overlaps with another
+            # For example b2 b3 J is equivalent to J b2 b3
+            for i in range(1, 15 - run_length):
+                for joker_pos in range(i + 1, i + run_length):
+                    runs.append(tuple(
+                        Tile("J", 0) if j == joker_pos else Tile(c, j)
+                        for j in range(i, i + run_length)
+                    ))
+
+            # Special case to put the joker in the 1st position for the 11, 12, 13 run
+            runs.append(tuple(
+                Tile("J", 0) if j == 14 - run_length else Tile(c, j)
+                for j in range(14 - run_length, 14)
+            ))
+
+            # Two jokers
+
+            for i in range(1, 15 - run_length):
+                for joker_pos_1 in range(i + 1, i + run_length - 1):
+                    for joker_pos_2 in range(joker_pos_1 + 1, i + run_length):
+                        runs.append(tuple(
+                            Tile("J", 0) if j == joker_pos_1 or j == joker_pos_2 else Tile(c, j)
+                            for j in range(i, i + run_length)
+                        ))
+
+            joker_pos_1 = 14 - run_length
+            for joker_pos_2 in range(joker_pos_1 + 1, 14):
+                runs.append(tuple(
+                    Tile("J", 0) if j == joker_pos_1 or j == joker_pos_2 else Tile(c, j) for
+                    j in range(14 - run_length, 14)
+                ))
+    return runs
+
+
+def create_groups():
+    groups: list[tuple[Tile, ...]] = []
+
+    for v in range(1, 14):
+        for cs in itertools.combinations(COLOURS, 3):
+            groups.append(tuple(Tile(c, v) for c in cs))
+
+        for cs in itertools.combinations(COLOURS, 2):
+            groups.append(tuple(Tile(c, v) for c in cs) + tuple([Tile("J", v)]))
+
+        # Groups of 3 tiles with 2 jokers already covered by runs
+
+        for cs in itertools.combinations(COLOURS, 4):
+            groups.append(tuple(Tile(c, v) for c in cs))
+
+        for cs in itertools.combinations(COLOURS, 3):
+            groups.append(tuple(Tile(c, v) for c in cs) + tuple([Tile("J", v)]))
+
+        for cs in itertools.combinations(COLOURS, 2):
+            groups.append(tuple(Tile(c, v) for c in cs) + tuple([Tile("J", v)] * 2))
+
+    return groups
 
 
 TILES = [Tile(c, v) for c in COLOURS for v in range(1, 14)] + [Tile("J", 0)]
 TILE_VALUES = np.array([t.value for t in TILES])
 JOKER_INDEX = len(TILES) - 1
 
+RUNS = create_runs()
+GROUPS = create_groups()
+SETS = RUNS + GROUPS
 
-class Rummi:
-    def __init__(self, joker_mode: JokerMode):
-        self.joker_mode = joker_mode
-        self.sets = self._create_runs() + self._create_groups()
-        self.set_to_index_map = {s: i for i, s in enumerate(self.sets)}
+SET_TO_INDEX_MAP = {s: i for i, s in enumerate(SETS)}
+SET_TILE_MATRIX = np.zeros((len(TILES), len(SETS)), int)
 
-        self.set_tile_matrix = np.zeros((len(TILES), len(self.sets)), int)
+for tiles, j in SET_TO_INDEX_MAP.items():
+    for t in tiles:
+        SET_TILE_MATRIX[t.get_i(), j] += 1
 
-        for tiles, j in self.set_to_index_map.items():
-            for t in tiles:
-                self.set_tile_matrix[t.get_i(), j] += 1
 
-    def _create_runs(self):
-        runs: list[tuple[Tile, ...]] = []
-        for c in COLOURS:
-            for run_length in [3, 4, 5]:
+def solve_cp_model(table_tiles_matrix, rack_tiles_matrix, table_sets_matrix, config: Config):
+    model = cp_model.CpModel()
 
-                # No Jokers
+    placed_sets_var = model.new_int_var_series("x", pd.Index(range(len(SETS))), 0, 2)
+    placed_tiles_var = model.new_int_var_series("y", pd.Index(range(len(TILES))), 0, 2)
+    unmodified_sets_var = model.new_int_var_series("z", pd.Index(range(len(SETS))), 0, 2)
 
-                for i in range(1, 15 - run_length):
-                    runs.append(tuple(Tile(c, j) for j in range(i, i + run_length)))
+    for i in range(len(TILES)):
+        model.add(
+            sum(SET_TILE_MATRIX[i, j] * placed_sets_var[j] for j in np.nonzero(SET_TILE_MATRIX[i])[0]) ==
+            table_tiles_matrix[i] +
+            placed_tiles_var[i])
 
-                # One joker
+        # Placed tiles cannot be more than rack tiles
+        model.add(placed_tiles_var[i] <= rack_tiles_matrix[i])
 
-                # Place jokers starting from the 2nd position, since using the first overlaps with another
-                # For example b2 b3 J is equivalent to J b2 b3
-                for i in range(1, 15 - run_length):
-                    for joker_pos in range(i + 1, i + run_length):
-                        runs.append(tuple(
-                            Tile("J", 0) if j == joker_pos else Tile(c, j)
-                            for j in range(i, i + run_length)
-                        ))
+    for j in range(len(SETS)):
+        # Unmodified sets are maximized in the optimization step, so it chooses the highest value <= both the
+        # table set and placed set, in other words unmodified_sets_var[j] = min(table_sets_matrix[j], placed_sets_var[j])
+        model.add_min_equality(unmodified_sets_var[j], [table_sets_matrix[j], placed_sets_var[j]])
 
-                # Special case to put the joker in the 1st position for the 11, 12, 13 run
-                runs.append(tuple(
-                    Tile("J", 0) if j == 14 - run_length else Tile(c, j)
-                    for j in range(14 - run_length, 14)
-                ))
+    if config.maximize_mode == MaximizeMode.VALUE_PLACED:
+        TILE_VALUES[JOKER_INDEX] = config.joker_value
+        model.maximize((placed_tiles_var * TILE_VALUES).sum() + config.rearrange_value * unmodified_sets_var.sum())
+    else:
+        # The paper does not include the change-minimization term in this version, but we add it anyway
+        model.maximize(placed_tiles_var.sum() + config.rearrange_value * unmodified_sets_var.sum())
 
-                # Two jokers
+    solver = cp_model.CpSolver()
+    solver.parameters.max_time_in_seconds = 10.0
 
-                for i in range(1, 15 - run_length):
-                    for joker_pos_1 in range(i + 1, i + run_length - 1):
-                        for joker_pos_2 in range(joker_pos_1 + 1, i + run_length):
-                            runs.append(tuple(
-                                Tile("J", 0) if j == joker_pos_1 or j == joker_pos_2 else Tile(c, j)
-                                for j in range(i, i + run_length)
-                            ))
+    status = solver.solve(model)
 
-                joker_pos_1 = 14 - run_length
-                for joker_pos_2 in range(joker_pos_1 + 1, 14):
-                    runs.append(tuple(
-                        Tile("J", 0) if j == joker_pos_1 or j == joker_pos_2 else Tile(c, j) for
-                        j in range(14 - run_length, 14)
-                    ))
-        return runs
+    if status not in (cp_model.OPTIMAL, cp_model.FEASIBLE):
+        raise RuntimeError("Invalid state")
 
-    def _create_groups(self):
-        groups: list[tuple[Tile, ...]] = []
+    sets_solution = solver.values(placed_sets_var).to_numpy()
+    tiles_solution = solver.values(placed_tiles_var).to_numpy()
 
-        for v in range(1, 14):
-            for cs in itertools.combinations(COLOURS, 3):
-                groups.append(tuple(Tile(c, v) for c in cs))
+    sets_on_table = np.column_stack((sets_solution.nonzero()[0], sets_solution[sets_solution.nonzero()]))
+    tiles_placed = np.column_stack((tiles_solution.nonzero()[0], tiles_solution[tiles_solution.nonzero()]))
 
-            for cs in itertools.combinations(COLOURS, 2):
-                groups.append(tuple(Tile(c, v) for c in cs) + tuple([Tile("J", v)]))
+    return sets_on_table, tiles_placed
 
-            # Groups of 3 tiles with 2 jokers already covered by runs
 
-            for cs in itertools.combinations(COLOURS, 4):
-                groups.append(tuple(Tile(c, v) for c in cs))
+def find_best_move(
+        table_sets: list[tuple[Tile, ...]],
+        rack_tiles: list[Tile],
+        config: Config
+) -> RummiResult:
+    table_tiles_matrix = np.zeros((len(TILES),), int)
+    for ts in table_sets:
+        for tile in ts:
+            table_tiles_matrix[tile.get_i()] += 1
 
-            for cs in itertools.combinations(COLOURS, 3):
-                groups.append(tuple(Tile(c, v) for c in cs) + tuple([Tile("J", v)]))
+    rack_tiles_matrix = np.zeros((len(TILES),), int)
+    for tile in rack_tiles:
+        rack_tiles_matrix[tile.get_i()] += 1
 
-            for cs in itertools.combinations(COLOURS, 2):
-                groups.append(tuple(Tile(c, v) for c in cs) + tuple([Tile("J", v)] * 2))
+    table_sets_matrix = np.zeros((len(SETS),), int)
+    for ts in table_sets:
+        # Incoming sets might have jokers at the start like "J a2 a3" which is not in our/the paper's sets, since they
+        # are made redundant by "a2 a3 J". In such cases we automatically rearrange them to our preferred format, since that is allowed.
+        if ts not in SET_TO_INDEX_MAP:
+            if ts[0].colour != "J":
+                raise RuntimeError("Invalid set")
+            ts = ts[1:] + tuple([ts[0]])
 
-        return groups
-
-    def _solve_cp_model(self, table_tiles_matrix, rack_tiles_matrix, table_sets_matrix, maximize_mode, joker_value):
-        set_tile_matrix = self.set_tile_matrix
-
-        tile_values = TILE_VALUES
-        tile_values[JOKER_INDEX] = joker_value
-
-        model = cp_model.CpModel()
-
-        x = model.NewIntVarSeries("x", pd.Index(range(len(self.sets))), 0, 2)
-        y = model.NewIntVarSeries("y", pd.Index(range(len(TILES))), 0, 2)
-        z = model.NewIntVarSeries("z", pd.Index(range(len(self.sets))), 0, 2)
-
-        for i in range(len(TILES)):
-            model.Add(
-                sum(set_tile_matrix[i, j] * x[j] for j in np.nonzero(set_tile_matrix[i])[0]) == table_tiles_matrix[i] +
-                y[i])
-
-            # Placed tiles (y) cannot be more than rack tiles (r)
-            model.Add(y[i] <= rack_tiles_matrix[i])
-
-        # z is the count of sets that are unmodified which we maximize in the optimization step
-        # z_j = min(x_j, w_j)
-        for j in range(len(self.sets)):
-            model.Add(z[j] <= x[j])
-            model.Add(z[j] <= table_sets_matrix[j])
-
-        if maximize_mode == MaximizeMode.VALUE_PLACED:
-            model.Maximize((y * tile_values).sum() + (1 / 40) * z.sum())
-        else:
-            # The paper does not include the change-minimization term in this version, but we add it anyway
-            model.Maximize(y.sum() + (1 / 40) * z.sum())
-
-        solver = cp_model.CpSolver()
-        solver.parameters.max_time_in_seconds = 10.0
-
-        status = solver.Solve(model)
-
-        if status not in (cp_model.OPTIMAL, cp_model.FEASIBLE):
-            raise RuntimeError("Invalid state")
-
-        x_solution = solver.Values(x).to_numpy()
-        y_solution = solver.Values(y).to_numpy()
-
-        sets_on_table = np.column_stack((x_solution.nonzero()[0], x_solution[x_solution.nonzero()]))
-        tiles_placed = np.column_stack((y_solution.nonzero()[0], y_solution[y_solution.nonzero()]))
-
-        return sets_on_table, tiles_placed
-
-    def find_best_move_with_parameters(
-            self,
-            table_sets: list[tuple[Tile, ...]],
-            rack_tiles: list[Tile],
-            maximize_mode=MaximizeMode.TILES_PLACED,
-            joker_value=0
-    ):
-        table_tiles_matrix = np.zeros((len(TILES),), int)
-        for ts in table_sets:
-            for tile in ts:
-                table_tiles_matrix[tile.get_i()] += 1
-
-        rack_tiles_matrix = np.zeros((len(TILES),), int)
-        for tile in rack_tiles:
-            rack_tiles_matrix[tile.get_i()] += 1
-
-        table_sets_matrix = np.zeros((len(self.sets),), int)
-        for ts in table_sets:
-            # Incoming sets might have jokers at the start like "J a2 a3" which is not in our/the paper's sets, since they
-            # are made redundant by "a2 a3 J". In such cases we automatically rearrange them to our preferred format, since that is allowed.
-            if ts not in self.set_to_index_map:
-                if ts[0].colour != "J":
-                    raise RuntimeError("Invalid set")
+            # Could be 2 jokers at the front so do it again
+            if ts[0].colour == "J":
                 ts = ts[1:] + tuple([ts[0]])
 
-                # Could be 2 jokers at the front so do it again
-                if ts[0].colour == "J":
-                    ts = ts[1:] + tuple([ts[0]])
+        table_sets_matrix[SET_TO_INDEX_MAP[ts]] += 1
 
-            table_sets_matrix[self.set_to_index_map[ts]] += 1
+    result_sets_matrix, result_tiles_matrix = solve_cp_model(
+        table_tiles_matrix,
+        rack_tiles_matrix,
+        table_sets_matrix,
+        config,
+    )
 
-        result_sets_matrix, result_tiles_matrix = self._solve_cp_model(
-            table_tiles_matrix,
-            rack_tiles_matrix,
-            table_sets_matrix,
-            maximize_mode,
-            joker_value
-        )
+    result_table_sets = []
+    for s in result_sets_matrix:
+        j, n = s[0], s[1]
+        for _ in range(n):
+            result_table_sets.append(SETS[j])
 
-        result_table_sets = []
-        for s in result_sets_matrix:
-            j, n = s[0], s[1]
-            for _ in range(n):
-                result_table_sets.append(self.sets[j])
+    placed_tiles = []
+    for t in result_tiles_matrix:
+        i, n = t[0], t[1]
+        for _ in range(n):
+            placed_tiles.append(TILES[i])
 
-        placed_tiles = []
-        for t in result_tiles_matrix:
-            i, n = t[0], t[1]
-            for _ in range(n):
-                placed_tiles.append(TILES[i])
+    remaining_tiles = rack_tiles.copy()
+    for tile in placed_tiles:
+        remaining_tiles.remove(tile)
 
-        remaining_tiles = rack_tiles.copy()
-        for tile in placed_tiles:
-            remaining_tiles.remove(tile)
+    result = RummiResult(sorted(result_table_sets), sorted(placed_tiles), sorted(remaining_tiles))
 
-        return sorted(result_table_sets), sorted(placed_tiles), sorted(remaining_tiles)
+    return result
+
+
+def find_best_move_strings(table_set_strings: list[str], rack_string: str, config: Config) -> RummiResult:
+    return find_best_move(
+        [tuple(Tile.from_str(s)) for s in table_set_strings],
+        Tile.from_str(rack_string),
+        config
+    )
--- test/test_main.py
+++ test/test_main.py
@@ -3,32 +3,10 @@ import unittest
 from collections import Counter
 from itertools import product
 
-from rummi import Tile, MaximizeMode, Rummi, JokerMode
+from rummi import Tile, MaximizeMode, JokerMode, find_best_move, Config, find_best_move_strings
 
 ALL_TILES_STRINGS = [c + str(val) for c, val in product("brya", range(1, 14))] * 2
 ALL_TILES_STRINGS_WITH_JOKERS = ALL_TILES_STRINGS + ["J"] * 2
-
-rummi = Rummi(JokerMode.FREE)
-
-def find_best_move_strings(table_set_strings, rack_string, maximize_mode, joker_value=0):
-    return rummi.find_best_move_with_parameters(
-        [tuple(Tile.from_str(s)) for s in table_set_strings],
-        Tile.from_str(rack_string),
-        maximize_mode,
-        joker_value
-    )
-
-
-def print_results(sets_on_table, tiles_placed, remaining_tiles):
-    print("Table sets:")
-    for s in sets_on_table:
-        print(s)
-
-    print("Tiles placed:")
-    print(tiles_placed)
-
-    print("Remaining tiles:")
-    print(remaining_tiles)
 
 
 class TestRummi(unittest.TestCase):
@@ -47,21 +25,22 @@ class TestRummi(unittest.TestCase):
                 for s in table_set_strings:
                     table_sets.append((tuple(Tile.from_str(s))))
         rack_tiles = Tile.from_str(rack_str)
-        result_sets_on_table, tiles_placed, remaining_tiles = rummi.find_best_move_with_parameters(table_sets, rack_tiles)
 
-        print_results(result_sets_on_table, tiles_placed, remaining_tiles)
+        config = Config(joker_mode=JokerMode.FREE, maximize_mode=MaximizeMode.TILES_PLACED)
+        result = find_best_move(table_sets, rack_tiles, config)
 
-        msg = f"sets: {result_sets_on_table} placed: {tiles_placed}; remaining: {remaining_tiles};"
+        print(result)
+
         self.assertEqual(
             sum(len(s) for s in table_sets) + len(rack_tiles) - expected_remaining,
-            sum(len(s) for s in result_sets_on_table), msg
+            sum(len(s) for s in result.table), result
         )
-        self.assertEqual(len(rack_tiles) - expected_remaining, len(tiles_placed), msg)
-        self.assertEqual(expected_remaining, len(remaining_tiles), msg)
+        self.assertEqual(len(rack_tiles) - expected_remaining, len(result.placed), result)
+        self.assertEqual(expected_remaining, len(result.remaining), result)
 
         if expected_unmodified is not None:
             before_sets = Counter(table_sets)
-            after_sets = Counter(result_sets_on_table)
+            after_sets = Counter(result.table)
             unmodified_sets = before_sets & after_sets
             print("Unmodified sets:", len(unmodified_sets))
 
@@ -141,18 +120,42 @@ class TestRummi(unittest.TestCase):
 
     def test_maximize_tiles(self):
         # Maximizing the number of tiles it should put the joker in the 5-run
-        table, placed, remaining = find_best_move_strings([], "a1 a2 a4 a5 a13 b13 J", MaximizeMode.TILES_PLACED)
-        self.assertCountEqual(Tile.from_str("a1 a2 J a4 a5"), placed)
+        config = Config(JokerMode.FREE, MaximizeMode.TILES_PLACED)
+        result = find_best_move_strings([], "a1 a2 a4 a5 a13 b13 J", config)
+        self.assertCountEqual(Tile.from_str("a1 a2 J a4 a5"), result.placed)
 
     def test_maximize_value(self):
         # Maximizing the value it should use the joker for the 13 group even though it is fewer tiles
-        table, placed, remaining = find_best_move_strings([], "a1 a2 a4 a5 a13 b13 J", MaximizeMode.VALUE_PLACED)
-        self.assertCountEqual(Tile.from_str("a13 b13 J"), placed)
+        config = Config(JokerMode.FREE, MaximizeMode.VALUE_PLACED, joker_value=0)
+        result = find_best_move_strings([], "a1 a2 a4 a5 a13 b13 J", config)
+        self.assertCountEqual(Tile.from_str("a13 b13 J"), result.placed)
 
     def test_maximize_value_plays_joker_when_30(self):
-        table, placed, remaining = find_best_move_strings([], "a1 a2 a3 J", MaximizeMode.VALUE_PLACED, 30)
-        self.assertCountEqual(Tile.from_str("a1 a2 a3 J"), placed)
+        config = Config(JokerMode.FREE, MaximizeMode.VALUE_PLACED, joker_value=30)
+        result = find_best_move_strings([], "a1 a2 a3 J", config)
+        self.assertCountEqual(Tile.from_str("a1 a2 a3 J"), result.placed)
 
     def test_maximize_value_does_not_play_joker_when_negative(self):
-        table, placed, remaining = find_best_move_strings([], "a1 a2 a3 J", MaximizeMode.VALUE_PLACED, -1)
-        self.assertCountEqual(Tile.from_str("a1 a2 a3"), placed)
+        config = Config(JokerMode.FREE, MaximizeMode.VALUE_PLACED, joker_value=-1)
+        result = find_best_move_strings([], "a1 a2 a3 J", config)
+        self.assertCountEqual(Tile.from_str("a1 a2 a3"), result.placed)
+
+    def test_minimize_rearrange(self):
+        # To ensure the model doesn't just happen to pick the right one, we test it can both minimize and maximize rearrangement
+
+        config = Config(JokerMode.FREE, MaximizeMode.TILES_PLACED, rearrange_value=1 / 40)
+        result = find_best_move_strings(["a1 a2 a3", "r1 r2 r3", "b1 b2 b3"], "a4 r4 b4", config)
+
+        expected_table = ["a1 a2 a3", "r1 r2 r3", "b1 b2 b3", "b4 r4 a4"]
+        self.assertCountEqual([tuple(Tile.from_str(s)) for s in expected_table], result.table)
+
+        config = Config(JokerMode.FREE, MaximizeMode.TILES_PLACED, rearrange_value=-1 / 40)
+        result = find_best_move_strings(["a1 a2 a3", "r1 r2 r3", "b1 b2 b3"], "a4 r4 b4", config)
+
+        expected_table = ["b1 r1 a1", "b2 r2 a2", "b3 r3 a3", "b4 r4 a4"]
+        self.assertCountEqual([tuple(Tile.from_str(s)) for s in expected_table], result.table)
+
+    def test_joker_locked_cannot_move(self):
+        config = Config(JokerMode.FREE, MaximizeMode.VALUE_PLACED, joker_value=0)
+        result = find_best_move_strings(["J a2 a3 a4"], "r1 r2", config)
+        print(result)
